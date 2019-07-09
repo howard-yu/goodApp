dac9cba7b4ef107e62f63db26d9140051b6ccc7a
howard-yu <y.shihhao@gmail.com>
2019-07-09 23:37:20 +0800

    Initial Commit

---
+++ goodApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goodApp
+//
+//  Created by Yu Shih-hao on 2019/7/9.
+//  Copyright © 2019 Yu Shih-hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
